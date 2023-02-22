f4b814c1548970007a0374b4aca4008a2f7f3d02
Adam Magomedov <adam5770125@gmail.com>
2023-02-22 18:22:09 +0300

    Добавил binding в FavoritesAuthorsAdapter

--- app/src/main/java/com/magomedov/githubrepos/adapters/FavoritesAuthorsAdapter.kt
+++ app/src/main/java/com/magomedov/githubrepos/adapters/FavoritesAuthorsAdapter.kt
@@ -4,11 +4,10 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.magomedov.githubrepos.R
+import com.magomedov.githubrepos.databinding.ItemFeaturedAuthorsBinding
 import com.magomedov.githubrepos.models.Favorites
 
 class FavoritesAuthorsAdapter(val itemAuthorsListener: AuthorsListener) :
@@ -17,10 +16,8 @@ class FavoritesAuthorsAdapter(val itemAuthorsListener: AuthorsListener) :
     var authors: List<Favorites> = emptyList()
 
     class AuthorsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val avatar: ImageView = itemView.findViewById(R.id.avatar)
-        val fulName: TextView = itemView.findViewById(R.id.name)
-        val electedLogin: TextView = itemView.findViewById(R.id.login)
-        val delete: ImageView = itemView.findViewById(R.id.delete)
+         val binding : ItemFeaturedAuthorsBinding = ItemFeaturedAuthorsBinding.bind(itemView)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AuthorsViewHolder {
@@ -35,12 +32,12 @@ class FavoritesAuthorsAdapter(val itemAuthorsListener: AuthorsListener) :
 
         Glide.with(holder.itemView)
             .load(authors.avatar)
-            .into(holder.avatar)
+            .into(holder.binding.avatar)
 
-        holder.fulName.setText(authors.nameProfile)
-        holder.electedLogin.setText(authors.login)
+        holder.binding.name.setText(authors.nameProfile)
+        holder.binding.login.setText(authors.login)
 
-        holder.delete.setOnClickListener(object : View.OnClickListener {
+        holder.binding.delete.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 itemAuthorsListener.onDelete(authors)
             }
