20486a2d2c5c4d972a73cc30943c5c421d31fd89
Adam Magomedov <adam5770125@gmail.com>
2023-02-22 19:12:33 +0300

    Заменил findViewById на binding в AboutAppFragment

--- app/src/main/java/com/magomedov/githubrepos/fragment/AboutAppFragment.kt
+++ app/src/main/java/com/magomedov/githubrepos/fragment/AboutAppFragment.kt
@@ -2,21 +2,26 @@ package com.magomedov.githubrepos.fragment
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.magomedov.githubrepos.R
+import com.magomedov.githubrepos.databinding.FragmentAboutAppBinding
 
 class AboutAppFragment: Fragment(R.layout.fragment_about_app) {
-
+  private var binding : FragmentAboutAppBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAboutAppBinding.bind(view)
 
-        val toolbar: Toolbar = view.findViewById(R.id.toolbar_about_app)
-        toolbar.setNavigationOnClickListener(object : View.OnClickListener{
+        binding!!.toolbarAboutApp.setNavigationOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 requireActivity().supportFragmentManager.popBackStack()
             }
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
