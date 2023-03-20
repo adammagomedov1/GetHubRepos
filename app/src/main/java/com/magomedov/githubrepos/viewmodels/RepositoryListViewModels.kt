8af272b52ddc680a72de4ee8008907012297af94
Adam Magomedov <adam5770125@gmail.com>
2023-03-20 21:34:58 +0300

    Добавил библиотеку Coroutine

--- app/src/main/java/com/magomedov/githubrepos/viewmodels/RepositoryListViewModels.kt
+++ app/src/main/java/com/magomedov/githubrepos/viewmodels/RepositoryListViewModels.kt
@@ -1,55 +1,33 @@
 package com.magomedov.githubrepos.viewmodels
 
-import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.magomedov.githubrepos.GitHubReposApplication
 import com.magomedov.githubrepos.models.Repository
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class RepositoryListViewModels : ViewModel() {
-    var getRepositoryCall: Call<List<Repository>> =
-        GitHubReposApplication.gitHubService.getRepositoryList()
 
-    val progressBarData = MutableLiveData<Boolean>()
+    val progressBarLiveData = MutableLiveData<Boolean>()
     val errorLiveData = MutableLiveData<Throwable>()
     val repositoriesLiveData = MutableLiveData<List<Repository>>()
-
-
+    
     fun loadRepositories() {
-        progressBarData.value = true
+        viewModelScope.launch {
+            try {
+                progressBarLiveData.value = true
 
-        getRepositoryCall.enqueue(object : Callback<List<Repository>> {
-            override fun onResponse(
-                call: Call<List<Repository>>,
-                response: Response<List<Repository>>
-            ) {
-                progressBarData.value = false
-                if (response.isSuccessful) {
-                    val repositoriesList: List<Repository>? = response.body()
-                    repositoriesLiveData.value = repositoriesList
-                }
+                val repositoryList: List<Repository> =
+                    GitHubReposApplication.gitHubService.getRepositoryList()
+
+                progressBarLiveData.value = false
+                repositoriesLiveData.value = repositoryList
+
+            } catch (e : Exception) {
+                progressBarLiveData.value = false
+                errorLiveData.value = e
             }
-
-            val sharedPreferences = GitHubReposApplication.context.getSharedPreferences(
-                "git_hub_preferences",
-                Context.MODE_PRIVATE
-            )
-
-            val repositoriesEditor: String? =
-                sharedPreferences.getString("repositories", null)
-
-            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
-                progressBarData.value = false
-                errorLiveData.value = t
-            }
-        })
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        getRepositoryCall.cancel()
+        }
     }
 }
